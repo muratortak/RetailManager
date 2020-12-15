7c47ea48db5fc23e5d7061bc20593773d4b9ef49
murat ortak <murat.ortak35@hotmail.com>
2020-12-14 22:38:14 -0500

    added user login

---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using RMDataManager.Library.DataAccess;
+using RMDataManagerLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        // GET: User/Details/5
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+
+    }
+}
